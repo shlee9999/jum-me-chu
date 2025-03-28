1f75779496da3b0568bef10572a59d8f33c30c98
seonghoon.lee <oklshop555@direa.co.kr>
2025-03-28 14:27:35 +0900

    refactor: MenuManager 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,13 @@
-import { useEffect, useState } from 'react';
-import { DragDropContext, Droppable, DropResult } from 'react-beautiful-dnd';
-import { Menu } from './components/Menu';
-import { cn } from './utils/cn';
-import { Roulette } from './components/Roulette';
-import { useMenuStore } from './store/menuStore';
 import { AddForm } from './components/AddForm';
+import { MenuManager } from './components/MenuManager';
+import { Roulette } from './components/Roulette';
+import { useInitAnimationFrame } from './hooks/useInitAnimationFrame';
+import { cn } from './utils/cn';
 
 function App() {
-  const {
-    activeMenus,
-    inactiveMenus,
-    setActiveMenus,
-    setInactiveMenus,
-    // addActiveMenu,
-  } = useMenuStore();
+  const { enabled } = useInitAnimationFrame();
 
-  const [enabled, setEnabled] = useState(false);
-
-  const onDragEnd = (result: DropResult) => {
-    const { source, destination } = result;
-
-    if (!destination) return;
-
-    if (
-      source.droppableId === destination.droppableId &&
-      source.index === destination.index
-    )
-      return;
-
-    if (
-      source.droppableId === 'active' &&
-      destination.droppableId === 'active'
-    ) {
-      const newActiveMenus = Array.from(activeMenus);
-      const [movedItem] = newActiveMenus.splice(source.index, 1);
-      newActiveMenus.splice(destination.index, 0, movedItem);
-      setActiveMenus(newActiveMenus);
-    } else if (
-      source.droppableId === 'inactive' &&
-      destination.droppableId === 'inactive'
-    ) {
-      const newInactiveMenus = Array.from(inactiveMenus);
-      const [movedItem] = newInactiveMenus.splice(source.index, 1);
-      newInactiveMenus.splice(destination.index, 0, movedItem);
-      setInactiveMenus(newInactiveMenus);
-    } else if (
-      source.droppableId === 'active' &&
-      destination.droppableId === 'inactive'
-    ) {
-      const newActiveMenus = Array.from(activeMenus);
-      const newInactiveMenus = Array.from(inactiveMenus);
-      const [movedItem] = newActiveMenus.splice(source.index, 1);
-      newInactiveMenus.splice(destination.index, 0, movedItem);
-      setActiveMenus(newActiveMenus);
-      setInactiveMenus(newInactiveMenus);
-    } else if (
-      source.droppableId === 'inactive' &&
-      destination.droppableId === 'active'
-    ) {
-      const newActiveMenus = Array.from(activeMenus);
-      const newInactiveMenus = Array.from(inactiveMenus);
-      const [movedItem] = newInactiveMenus.splice(source.index, 1);
-      newActiveMenus.splice(destination.index, 0, movedItem);
-      setInactiveMenus(newInactiveMenus);
-      setActiveMenus(newActiveMenus);
-    }
-  };
-
-  useEffect(() => {
-    const animation = requestAnimationFrame(() => setEnabled(true));
-
-    return () => {
-      cancelAnimationFrame(animation);
-      setEnabled(false);
-    };
-  }, []);
-
-  if (!enabled) {
-    return null;
-  }
+  if (!enabled) return null;
 
   return (
     <div
@@ -92,42 +21,7 @@ function App() {
 
       <Roulette />
       <AddForm />
-
-      <div className='flex w-full justify-center gap-10 mt-6'>
-        <DragDropContext onDragEnd={onDragEnd}>
-          <div className='w-2/5'>
-            <h3 className='text-center text-blue-600 font-semibold mb-3'>
-              맛집 리스트
-            </h3>
-            <Droppable droppableId='active'>
-              {(provided) => (
-                <Menu
-                  menuOptions={activeMenus}
-                  provided={provided}
-                  className='bg-blue-100 border border-blue-600 rounded-md p-3'
-                  droppableId='active'
-                />
-              )}
-            </Droppable>
-          </div>
-
-          <div className='w-2/5'>
-            <h3 className='text-center text-red-600 font-semibold mb-3'>
-              오늘은 안갈래
-            </h3>
-            <Droppable droppableId='inactive'>
-              {(provided) => (
-                <Menu
-                  menuOptions={inactiveMenus}
-                  provided={provided}
-                  className='bg-red-100 border border-red-600 rounded-md p-3'
-                  droppableId='inactive'
-                />
-              )}
-            </Droppable>
-          </div>
-        </DragDropContext>
-      </div>
+      <MenuManager />
     </div>
   );
 }
